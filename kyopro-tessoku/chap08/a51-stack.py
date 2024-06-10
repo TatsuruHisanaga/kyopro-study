b037a704bdc614e6443751738cfdc3947698353e
Tatsuru Hisanaga <tatsu72x@gmail.com>
2024-06-10 11:51:54 +0900

    Refactor: 可読性の向上

--- kyopro-tessoku/chap08/a51-stack.py
+++ kyopro-tessoku/chap08/a51-stack.py
@@ -1,15 +1,25 @@
-# 入力
+# クエリの数を入力として受け取る
 Q = int(input())
+
+# 各クエリを保存するためのリストを初期化
 queries = [None] * Q
+
+# 各クエリを入力として受け取り、リストに保存
 for i in range(Q):
     queries[i] = input().split()
 
+# スタックを初期化
+stack = []
+
 # クエリの処理
-S = []
-for q in queries:
-    if q[0] == "1":
-        S.append(q[1])
-    elif q[0] == "2":
-        print(S[-1])
-    elif q[0] == "3":
-        S.pop()
+for query in queries:
+    command = query[0]
+
+    if command == "1":
+        book_title = query[1]
+        stack.append(book_title)
+    elif command == "2":
+        print(stack[-1])
+    elif command == "3":
+        stack.pop()
+
