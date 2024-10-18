edc42081ecedd4d11aca4ccc0e670e137957d467
Tatsuru Hisanaga <tatsu72x@gmail.com>
2024-10-18 16:57:06 +0900

    Create binary-search.py

---
+++ binary-search.py
@@ -0,0 +1,15 @@
+# 退数時間で探索できる
+def binary_search(list, item):
+  low = 0
+  high = len(list) - 1
+
+  while low <= high:
+    mid = (low + high) // 2
+    guess = list[mid]
+    if guess == item:
+      return mid
+    if guess > item:
+      high = mid - 1
+    else:
+      low = mid + 1
+  return None
